d3112322c414c66d545e281343308e769f51c13b
yyz <xiaoyaovo@qq.com>
2022-04-10 15:56:53 +0800

    finished v0.1

--- main.go
+++ main.go
@@ -1,11 +1,70 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"flag"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+var localHost = flag.String("l", "0.0.0.0:9001", "需要绑定本地的http服务HOST")
+var forwardHost = flag.String("r", "www.baidu.com", "需要转发的http服务HOST")
+
+func parseRequest(c *gin.Context) {
+	resp := forward(c.Request)
+	c.Status(resp.StatusCode)
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	if err = resp.Body.Close();err != nil {
+		panic(err)
+	}
+	for key, value := range resp.Header {
+		c.Header(key, value[0])
+	}
+	c.String(resp.StatusCode, string(content))
+}
+
+func forward(req *http.Request) (resp *http.Response) {
+	fmt.Println("Forwarding to : " + *forwardHost + "......")
+	HTTPVersion := "http"
+	if req.TLS != nil {
+		HTTPVersion = "https"
+	}
+
+	targetURL := url.URL{
+		Host: *forwardHost,
+		Scheme: HTTPVersion,
+		Path: req.URL.Path,
+		RawQuery: req.URL.RawQuery,
+	}
+	request := http.Request{
+		URL: &targetURL,
+		Host: *forwardHost,
+		Method: req.Method,
+		Header: req.Header,
+		Body: req.Body,
+	}
+	client := &http.Client{}
+	resp, err := client.Do(&request)
+	if err != nil {
+		log.Println(err)
+	}
+	return
+}
 
 func main() {
+	flag.Parse()
+	flag.Args()
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-	err := r.Run("0.0.0.0:8081")
-	if err != nil {
-		return 
+	r.Any("/*any", parseRequest)
+	fmt.Println("开始运行http服务：" + *localHost)
+	if err := r.Run(*localHost);err != nil {
+		return
 	}
 }
